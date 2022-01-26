56ac6f65f2b16124c1bd14112d0ddcb22d8d9bb6
gkdudqo <86238349+gkdudqo@users.noreply.github.com>
2022-01-27 00:17:01 +0900

    김하영 : 220126 숙제 커밋

---
+++ HomeWork/HomeWork220126/HomeWork220126.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+
+class MyInt
+{
+public:
+
+    int Value;
+
+public:
+    MyInt operator++()
+    {
+        Value += 1;
+
+        return *this;
+    }
+
+    MyInt operator++(int)
+    {
+        MyInt temp(*this);
+        Value += 1;
+
+        return temp;
+    }
+    MyInt operator--()
+    {
+        Value -= 1;
+
+        return *this;
+    }
+    MyInt operator--(int)
+    {
+        MyInt temp(*this);
+        Value -= 1;
+
+        return temp;
+    }
+public:
+    MyInt(int _Value)
+        : Value(_Value)
+    {
+
+    }
+};
+
+int main()
+{
+    int Value = 0;
+    int Result = 0;
+
+    Result = ++Value;
+
+    Result = 0;
+    Result = Value++;
+
+
+    MyInt MyValue = 0;
+    MyInt MyResult = 0;
+
+    // ++ 쓰지마세요
+    MyResult = ++MyValue;
+    MyResult = 0;
+    MyResult = MyValue++;
+
+    MyResult = 0;
+    MyResult = --MyValue;
+    MyResult = 0;
+    MyResult = MyValue--;
+}
